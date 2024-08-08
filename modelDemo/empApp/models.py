5e2fa4e34224f66c06796b8d68d103994ca3468b
ShashankShekhar07 <shanky74022@gmail.com>
2024-08-08 15:51:45 +0530

    ORM Relationship

--- modelDemo/empApp/models.py
+++ modelDemo/empApp/models.py
@@ -7,3 +7,30 @@ class Employee(models.Model):
     salary=models.FloatField()
     email=models.CharField(max_length=35)
 
+
+class Programmer(models.Model):
+    name=models.CharField(max_length=20)
+    sal=models.IntegerField()
+
+class Project(models.Model):
+    name=models.CharField(max_length=30)
+    programmers=models.ManyToManyField(Programmer)
+
+class Customer(models.Model):
+    name=models.CharField(max_length=30)
+
+class PhoneNumber(models.Model):
+    type=models.CharField(max_length=10)
+    number=models.CharField(max_length=10)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Person(models.Model):
+    firstName=models.CharField(max_length=20)
+    lastName=models.CharField(max_length=20)
+    age=models.IntegerField()
+
+class License(models.Model):
+    type=models.CharField(max_length=10)
+    validFrom=models.DateField()
+    validTo=models.DateField()
+    person=models.OneToOneField(Person,on_delete=models.CASCADE)
